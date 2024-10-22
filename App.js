bb40636811f0b4baf337a021cfac5448ad725f11
Moemen Khchim madi <madokakhchimadi@gmail.com>
2024-10-22 16:01:05 +0100

    fifth page

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import FirstPage from './components/firstPage'; 
-import SecondPage from './components/secondPage'; 
+import FirstPage from './components/firstPage';
 import ThirdPage from './components/thirdPage'; 
+import SecondPage from './components/SecondPage'; 
+import FourthPage from './components/FourthPage';
+import FifthPage from './components/fifthPage';
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
@@ -12,6 +14,9 @@ const App = () => {
         <Stack.Screen name="FirstPage" component={FirstPage} />
         <Stack.Screen name="SecondPage" component={SecondPage} />
         <Stack.Screen name="ThirdPage" component={ThirdPage} />
+        <Stack.Screen name="FourthPage" component={FourthPage} />
+        <Stack.Screen name="FifthPage" component={FifthPage} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
